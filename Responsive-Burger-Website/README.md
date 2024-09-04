c53b36c981423711f01d0f58df21ed71c0b9ae87
Sambit Kumar Nanda <50833547+SAMBIT20@users.noreply.github.com>
2024-09-04 20:50:07 +0530

    Love Every Bite

---
+++ Responsive-Burger-Website/README.md
@@ -0,0 +1,11 @@
+# Responsive Burger Website
+
+### Responsive Burger Website
+
+- Responsive Burger Website Design Using HTML CSS And JavaScript.
+- Contains animations when scrolling.
+- Smooth scrolling in each section.
+- Developed first with the Mobile First methodology, then for desktop.
+- Compatible with all mobile devices with a beautiful and pleasant user interface.
+
+![preview img](/preview.png)
---
+++ Responsive-Burger-Website/assets/js/main.js
@@ -0,0 +1,104 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById("nav-menu");
+const navToggle = document.getElementById("nav-toggle");
+const navClose = document.getElementById("nav-close");
+
+// Menu Show
+if (navToggle) {
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
+
+// Menu Hidden
+if (navClose) {
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll(".nav__link");
+
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
+};
+
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () => {
+  const header = document.getElementById("header");
+  // Add a class if the bottom offset is greater than 50 of the viewport
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+
+window.addEventListener("scroll", shadowHeader);
+
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () => {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 350 viewport height, add the show-scroll class to the <a> tag with the scrollup class
+  this.scrollY >= 350
+    ? scrollUp.classList.add("show-scroll")
+    : scrollUp.classList.remove("show-scroll");
+};
+
+window.addEventListener("scroll", scrollUp);
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
+
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+
+window.addEventListener("scroll", scrollActive);
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2500,
+  delay: 300,
+  // reset: true, // Animations repeat
+});
+
+sr.reveal(`.home__data, .footer`);
+sr.reveal(`.home__dish`, { delay: 500, distance: "100px", origin: "bottom" });
+sr.reveal(`.home__burger`, {
+  delay: 1200,
+  distance: "100px",
+  duration: 1500,
+});
+sr.reveal(`.home__ingredient`, {
+  delay: 1600,
+  interval: 100,
+});
+sr.reveal(`.recipe__img, .delivery__img, .contact__image`, {
+  origin: "left",
+});
+sr.reveal(`.recipe__data, .delivery__data, .contact__data`, {
+  origin: "right",
+});
+sr.reveal(`.popular__card`, {
+  interval: 100,
+});
